16b985059479bb1948ac677a35701a67711fcb15
Giang Pham <giangttpham@gmail.com>
2023-08-19 15:53:25 -0700

    Updated GET endpoint, added models, allowed cors

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
 from fastapi import FastAPI, HTTPException
+from dataclasses import dataclass
 import httpx
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+@dataclass
+class Book:
+    book_id: str
+    title: str
+    authors: [str]
+    description: str
+    image_links: {}
+
+    def __init__(self, book_id, title, authors, description, image_links):
+        self.book_id = book_id
+        self.title = title
+        self.authors = authors
+        self.description = description
+        self.image_links = image_links
+
+
+@dataclass
+class BookSearchResponse:
+    totalItems: int
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,12 +49,13 @@ async def say_hello(name: str):
 
 GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes"
 
-@app.get("/books/{search_string}")
-async def get_book(search_string: str, skip: int = 0, limit: int = 10):
-    query = search_string.replace(" ","+")
+
+@app.get("/books/")
+async def get_book(keywords: str, skip: int = 0, limit: int = 10):
+    # query = search_string.replace(" ", "+")
 
     params = {
-        "q": query,
+        "q": keywords,
         "startIndex": skip,
         "maxResults": limit
     }
@@ -34,20 +68,17 @@ async def get_book(search_string: str, skip: int = 0, limit: int = 10):
 
         books = []
 
-        items = data["items"]
         if "items" in data and data["items"]:
             for item in data["items"]:
                 book_info = item["volumeInfo"]
+
+                book_id = item["id"]
                 title = book_info.get("title", "Unknown Title")
                 authors = book_info.get("authors", ["Unknown Author"])
                 description = book_info.get("description", "No description available")
+                image_links = book_info.get("imageLinks", {})
 
-                images = book_info.get("imageLinks", [])
-
-                books.append({"title": title,
-                    "authors": authors,
-                    "description": description,
-                    "image_links": images})
+                books.append(Book(book_id, title, authors, description, image_links))
 
             return books
         else:
