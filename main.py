0c366abe160cb23120be60ba0b38f8afd9444a22
Giang Pham <giangttpham@gmail.com>
2023-08-20 12:55:55 -0700

    Mocked httpx response for testing and cleaned up code

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 import httpx
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
 
 @dataclass
 class Book:
@@ -22,10 +20,11 @@ class Book:
         self.image_links = image_links
 
 
-@dataclass
-class BookSearchResponse:
-    totalItems: int
+app = FastAPI()
 
+GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes"
+
+# Allow CORS
 origins = ["*"]
 
 app.add_middleware(
@@ -37,49 +36,28 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes"
-
-
 @app.get("/books/")
-async def get_book(keywords: str, skip: int = 0, limit: int = 10):
-    # query = search_string.replace(" ", "+")
-
+async def get_books(keywords: str, skip: int = 0, limit: int = 10):
     params = {
         "q": keywords,
         "startIndex": skip,
         "maxResults": limit
     }
 
-    # params = f'intitle:${book_name}'
-
     async with httpx.AsyncClient() as client:
         response = await client.get(GOOGLE_BOOKS_API_URL, params=params)
         data = response.json()
 
         books = []
-
         if "items" in data and data["items"]:
             for item in data["items"]:
-                book_info = item["volumeInfo"]
-
                 book_id = item["id"]
+                book_info = item["volumeInfo"]
                 title = book_info.get("title", "Unknown Title")
                 authors = book_info.get("authors", ["Unknown Author"])
                 description = book_info.get("description", "No description available")
                 image_links = book_info.get("imageLinks", {})
-
                 books.append(Book(book_id, title, authors, description, image_links))
-
             return books
         else:
             raise HTTPException(status_code=404, detail="Book not found")
--- test_main.py
+++ test_main.py
@@ -1,13 +1,38 @@
-from fastapi.testclient import TestClient
-
-from main import app
+import json
+from unittest.mock import patch
+import httpx
 import pytest
 
-client = TestClient(app)
+import main
 
 
-class TestBookApi:
-    def test_get_book(self):
-        response = client.get("/books/frankeinstein")
+class TestBookSearchApi:
+    @pytest.mark.anyio
+    @patch("main.httpx.AsyncClient.get")
+    async def test_get_books_ok_response(self, mock_httpx_get):
+        # Mock Google Books API response
+        response_ojb = {
+            "kind": "books#volumes",
+            "totalItems": 14976,
+            "items": [
+                {
+                    "id": "hWgfPQAACAAJ",
+                    "volumeInfo": {
+                        "title": "Flowers for Algernon - Teacher Guide",
+                        "authors": [
+                            "Anc Staff Novel Units"
+                        ],
+                        "description": "Activities to be used in the classroom to accompany the reading of Flowers for Algernon by Daniel Keyes.",
+                        "imageLinks": {
+                            "smallThumbnail": "http://books.google.com/books/content?id=hWgfPQAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+                            "thumbnail": "http://books.google.com/books/content?id=hWgfPQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+                        }
+                    }
+                }
+            ]
+        }
+        mock_httpx_get.return_value = httpx.Response(200, content=json.dumps(response_ojb))
 
-        assert response.status_code == 200
+        result = await main.get_books("some keywords")
+        assert len(result) == 1
+        assert result[0].title == response_ojb["items"][0]["volumeInfo"].get("title", "")
